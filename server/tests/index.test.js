4d58aa203482537098ee7b9365e1c92684fd5445
Daniel Zelaya <wupaz9@gmail.com>
2017-10-31 18:34:39 -0600

    Switch from mocha to jest

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,10 +1,9 @@
-const { expect } = require("chai");
 const { Server } = require("hapi");
 
 const server = require("../index");
 
 describe("Server Index", () => {
   it("should export a hapi server", () => {
-    expect(server).instanceof(Server);
+    expect(server).toBeInstanceOf(Server);
   });
 });
